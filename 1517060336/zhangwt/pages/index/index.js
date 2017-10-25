8656d946b9731eee596d5610107892e351b1e06a
unknown <867858664@qq.com>
2017-10-25 09:55:57 +0800

    #81 张伟涛的期末作业

---
+++ 1517060336/zhangwt/pages/index/index.js
@@ -0,0 +1,107 @@
+//index.js
+//获取应用实例
+// pages/comp/scroll.js
+var base64 = require("../example/images/base64");
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isEnd: false,
+    messages: [{
+      name: "男款双肩",
+      last: "123456789"
+    }, {
+      name: "女款双肩",
+      last: "18745745452"
+    }, {
+      name: "钱包",
+      last: "19245774565"
+    }, {
+      name: "旅行箱",
+      last: "17454457744！"
+    }, {
+      name: "儿童",
+      last: "17724546584"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.setData({
+      icon20: base64.icon20,
+      icon60: base64.icon60
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  onEnd: function () {
+    console.log("onEnd")
+    var that = this;
+    var data = that.data;
+    that.setData({
+      isEnd: true /*,
+      messages: data.messages.concat([{
+        name: "Tom",
+        last: "Hello!"
+      }, {
+        name: "john",
+        last: "yes."
+      }]) */
+    });
+
+  }
+})
+
